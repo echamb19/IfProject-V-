e7176a7551223ed5a1c7c5a3fb8dcb159c107ad1
Eden Chambers <EdenChambers5@gmail.com>
2018-09-20 13:05:59 -0600

    I set it up so that I can use the JOptionPane etc. style of printing. I also acknowledged confusion.

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -1,11 +1,16 @@
 package controller;
 
+/**
+ * Allows you to use the pop-up windows to type into them and get responses.
+ */
+import javax.swing.JOptionPane;
+
 public class IfController
 {
 	/**
 	 * Builds instance of the Controller
 	 */
-//	public Controller()
+//	public Controller() <-- Not sure what I'm doing wrong, but one step at a time. 
 	{
 
 	}
