f5d317e1d87d0348f1ed08b67844a05f74c337bc
Eden Chambers <EdenChambers5@gmail.com>
2018-09-20 12:10:32 -0600

    I made the Data Member Section and the Constructors.
    DM Section-at the top
       ex. lines 5-10
    Constructors- After that
       ex. lines 17-25

--- src/model/Library.java
+++ src/model/Library.java
@@ -2,5 +2,26 @@ package model;
 
 public class Library
 {
-
+	private int books; 
+	private int people; 
+	private String librarianName;
+	private double finishedBooks;
+	private boolean isOpen;
+	private String libraryName;
+	
+	public Library()
+	{
+		
+	}
+	
+	 public Library(int books, int people, String librarianName, double finishedBooks, boolean isOpen, String libraryName)
+	 {
+		 this.books = books;
+		 this.people = people; 
+		 this.librarianName = librarianName; 
+		 this.libraryName = libraryName;
+		 this.isOpen = isOpen;
+		 this.librarianName = librarianName;		 
+	 }
+	 
 }
