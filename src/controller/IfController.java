e7449bdfa87b9bef7d1c0b8201b98d5ecda1e6f6
Eden Chambers <EdenChambers5@gmail.com>
2018-09-20 12:53:40 -0600

    I added javadoc to my code and put in the getters and setters.
    Public controller in the controller isn't working. I'll work on fixing it before the next comit.

--- src/controller/IfController.java
+++ src/controller/IfController.java
@@ -2,6 +2,13 @@ package controller;
 
 public class IfController
 {
+	/**
+	 * Builds instance of the Controller
+	 */
+//	public Controller()
+	{
+
+	}
 	public void start() 
 	{
 		
--- src/model/Library.java
+++ src/model/Library.java
@@ -9,6 +9,7 @@ public class Library
 	private boolean isOpen;
 	private String libraryName;
 	
+	// v This is a javadoc used to put info in about things and you get the info when you hover over it. 
 	/**
 	 * Default constructor for a Library. 
 	 * Initializes all variables to valid, but "bad" values. 
@@ -20,9 +21,19 @@ public class Library
 		this.people = -75; 
 		this.librarianName = "NotAName"; 
 		this.finishedBooks = 7.5;
-		
+		this.isOpen = false; 
+		this.libraryName = "NoNameLibrary";
 	}
 	
+	/**
+	 * Initializes the books etc. with a value for the stuff.
+	 * @param books 			The number of books.
+	 * @param people 			The number of people.
+	 * @param librarianName 	The name of the Librarian. 
+	 * @param finishedBooks 	The number of books I have finished. 
+	 * @param isOpen 			This tells you whether the library is open. 
+	 * @param libraryName 		What the name of the library is. 
+	 */
 	 public Library(int books, int people, String librarianName, double finishedBooks, boolean isOpen, String libraryName)
 	 {
 		 this.books = books;
@@ -32,5 +43,82 @@ public class Library
 		 this.isOpen = isOpen;
 		 this.librarianName = librarianName;		 
 	 }
+	 
+	 public int getBooks()
+	 {
+		 return books;
+	 }
+	 
+	 public int getPeople()
+	 {
+	 return people;
+	 }
+
+	 public String getLibrarianName()
+	 {
+		 return librarianName;
+	 }
+	 
+	 public double getFinishedBooks()
+	 {
+		 return finishedBooks;
+	 }
+	 
+	 public boolean getIsOpen()
+	 {
+		 return isOpen;
+	 }
+	 
+	 public String getLibraryName()
+	 {
+		 return libraryName;
+	 }
+	 
+	 
+	 public void setBooks(int books)
+	 {
+		 this.books = books;
+	 }
+	 
+	 public void setPeople(int people)
+	 {
+		 this.people = people; 
+	 }
+	 
+	 public void setLibrarianName(String librarianName)
+	 {
+		 this.librarianName = librarianName; 
+	 }
+	 
+	 public void setFinishedBooks(double finishedBooks)
+	 {
+		 this.finishedBooks = finishedBooks;
+	 }
+	 
+	 public void setIsOpen (boolean isOpen)
+	 {
+		 this.isOpen = isOpen;
+	 }
+	 
+	 public void setLibraryName(String libraryName)
+	 {
+		 this.libraryName = libraryName;
+	 }
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
+	 
 	 
 }
